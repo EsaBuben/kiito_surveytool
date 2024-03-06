c852bf2f221e89cfc0e891b560a7f920cc2b8397
simokuos <simo.kuosmanen@student.lab.fi>
2024-03-06 11:08:04 +0200

    Taulukko pohja

---
+++ kiito_surveytool/src/tulokset/test.tsx
@@ -0,0 +1,19 @@
+/*
+tasks:
+- container body
+- otsikko palkki / search mui
+- tab choice bar / search mui
+- taulukko / search mui
+- latausnappi
+*/
+import {Taulukko} from './taulukko';
+
+export function Tulokset(props:any){
+
+  return( <div>
+    <Taulukko />
+
+
+  </div>);
+
+}
