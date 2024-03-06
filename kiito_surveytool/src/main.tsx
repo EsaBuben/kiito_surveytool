4e496bbe1fa6aade8456cd8e19148e06952ce887
VuorioN <116265805+VuorioN@users.noreply.github.com>
2024-03-06 21:35:56 +0200

    Add files for question page

--- kiito_surveytool/src/main.tsx
+++ kiito_surveytool/src/main.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+//import App from './App.tsx'
+import { TitleBar } from './TitleBar.tsx'
+import { TitleButton } from './TitleButton.tsx'
+import TypographyTheme from './Instruction.tsx'
+import Questions from './Questions.tsx'
+import { LowerButtons } from './LowerButtons.tsx'
+
+
+//Palaa etusivulle-nappi
+//Kyselyn aihe
+//Jatka, Palaa, Tuloksiin
+//Kysymys ja pallurat
+//Kysymysmerkki-hoverin sisältö
+//Ohjeet käyttäjälle ja kysymysmerkki-hover
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <TitleButton />
+    <TitleBar />
+    <TypographyTheme />
+    <Questions />
+    <LowerButtons />
   </React.StrictMode>,
 )
