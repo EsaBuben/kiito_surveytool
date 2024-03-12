408e9160886a2f8374277c785ea15f1da2659a86
VuorioN <116265805+VuorioN@users.noreply.github.com>
2024-03-12 11:01:47 +0200

    Add question page files

---
+++ kiito_surveytool/src/components/TitleBar.tsx
@@ -0,0 +1,32 @@
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+export function TitleBar(props:any){
+  const Title : string = "1. Materiaalien kestävä käyttä"
+  const subTitle : string = "Materiaalen valinta ja vähentäminen"
+
+  return(<Paper
+    sx={{
+      background:"#039BE5",
+      color:"white",
+      padding: "10px 5px",
+      borderRadius: 6,
+      width:"80%",
+      margin:"auto"
+    }}
+    >
+    <Grid container justifyContent="flex-end" >
+      <Grid item sx={{margin:"auto"}} >
+        <Grid item>
+          <Typography sx={{textAlign:"center", fontWeight:"bold"}} variant="h5">{Title}</Typography>
+        </Grid>
+        <Grid item>
+          <Typography sx={{textAlign:"center", fontWeight:"bold"}} variant="subtitle1">{subTitle}</Typography>
+        </Grid>
+      </Grid>
+      <Grid item >
+        <img style={{width:"50px", height:"50px",paddingRight:"30px"}} />
+      </Grid>
+    </Grid>
+  </Paper>)
+}
