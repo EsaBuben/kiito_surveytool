583a61f792de8f91714c944aec27803642bd3549
Esa <esa.buben@student.lab.fi>
2024-03-14 18:10:24 +0200

    Json ja layout
    Json sisältöä tuotu eri komponentteihin. Jotain reformatointia.

--- kiito_surveytool/src/components/Etusivu.tsx
+++ kiito_surveytool/src/components/Etusivu.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Stack } from '@mui/material'
-import { Header, Footer, Content } from '.'
+import { Header, Content } from '.'
 
 const Etusivu = () => {
   return (
