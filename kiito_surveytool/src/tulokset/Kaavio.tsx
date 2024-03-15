65be850d36f5d0be4a0387d0b2c2a1a4796b227e
simokuos <simo.kuosmanen@student.lab.fi>
2024-03-15 12:37:21 +0200

    kaavio basic

--- kiito_surveytool/src/tulokset/Kaavio.tsx
+++ kiito_surveytool/src/tulokset/Kaavio.tsx
@@ -1,7 +1,64 @@
+import {
+  Chart as ChartJS,
+  LineElement,
+  PointElement,
+  Tooltip,
+  Legend,
+  RadialLinearScale
+} from 'chart.js/auto';
+//Tooltip, Legend
+import {Radar} from 'react-chartjs-2';
+
+ChartJS.register(
+  LineElement,
+  PointElement,
+  Tooltip,
+  Legend,
+  RadialLinearScale
+)
+
 export function Kaavio(props:any){
-  return(<div>
-    
-    <p>Kaavio</p>
+
+
+  const data = {
+    labels: ["mon", "tue","wed","thur","fri", "sat"], //tasot alaotsikkot
+    datasets:[{
+      label:"weekdays",  //keskiarvo
+      data: [2,3,4,1,5,3], //arvot
+    }]
+  };
+
+  const options = {
+    plugins: {
+       legend: {
+         display: false,
+       },
+     },
+     scales: {
+        r: {
+            angleLines: {
+                display: false
+            },
+            suggestedMin: 0,
+            suggestedMax: 5,
+            ticks:{
+              stepSize: 1,
+              showLabelBackdrop: false,
+            },
+
+            }
+          },
+
+  }
+
+  return(<div >
+
+    <Radar
+    data={data}
+    options={options}
+    >
+
+    </Radar>
   </div>
   )
 }
