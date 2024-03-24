16c3502e8f8308399cb641f961b507476a69f74d
Niko <niko.vuorio@student.lab.fi>
2024-03-24 19:56:26 +0200

    Form state to array

--- kiito_surveytool/src/components/Questions.tsx
+++ kiito_surveytool/src/components/Questions.tsx
@@ -3,8 +3,9 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
-import json from '../test.json';
 import { useState } from 'react';
+import vittusaatana from './vittusaatana.json'
+import { testi } from './KyselyContent';
 
 
 
@@ -16,35 +17,39 @@ export default function Questions(props : any) {
   //jiisonnista sisältö tännekki
   props.sivu
 
-  let [select, setSelect] = useState("")
-  function handleChange(event: any) {
-    setSelect(event.target.value);
+
+  function handleChange(e: any, index: number) {
+    const newVal = parseInt(e.target.value)
+    const updatedAns = [...ans]
+    updatedAns[testi - 1][index] = newVal
+    setAns(updatedAns)
  }
 
-  let ques : string[] = []
 
-  for(let i = 0; i < json.sivut[0].kategoriat.length; i++)
+  var ques : any[][]
+  let [ans, setAns] = useState<number[][]>(() => {return Array.from({length: vittusaatana.alaotsikko.length}, () => Array.from({length: 3}, () => 0))})
+  ques = []
+  for(var i = 0; i < vittusaatana.alaotsikko.length; i++)
   {
-    for(let j = 0; j < json.sivut[0].kategoriat[i].tasot.length; j++)
+    ques[i] = []
+    for(var j = 0; j < vittusaatana.alaotsikko[i].kysymykset.length; j++)
     {
-      for(let k = 0; k < json.sivut[0].kategoriat[i].tasot[j].kysymykset.length; k++)
-      {
-        ques.push(json.sivut[0].kategoriat[i].tasot[j].kysymykset[k])
-      }
+      ques[i].push(vittusaatana.alaotsikko[i].kysymykset[j])
     }
   }
-
+console.log(ans)
   return (
     <>
-    <div style={{display: "flex", width: "80%",  fontFamily: "arial", marginBottom: '30px'}}>
+    {ques[testi-1].map((x, index) => 
+    <div key={index} style={{display: "flex", width: "80%",  fontFamily: "arial", marginBottom: '30px'}}>
     <FormControl sx={{flexDirection: 'row'}}>
-      <FormLabel id="Q1" sx={{width:'50%'}} >{ques[0]}</FormLabel>
+      <FormLabel sx={{width:'50%'}} >{x}</FormLabel>
       <RadioGroup
         row
         aria-labelledby="demo-row-radio-buttons-group-label"
         name="row-radio-buttons-group"
-        value={select}
-        onChange={handleChange}
+        value={ans[testi-1][index]}
+        onChange={(e) => handleChange(e, index)}
       >
         <FormControlLabel value="1" control={<Radio />} label="1" labelPlacement='bottom' />
         <FormControlLabel value="2" control={<Radio />} label="2" labelPlacement='bottom' />
@@ -54,42 +59,7 @@ export default function Questions(props : any) {
       </RadioGroup>
     </FormControl>
     </div>
-
-
-    <div style={{display: "flex", width: "80%",  fontFamily: "arial", marginBottom: '30px'}}>
-    <FormControl sx={{flexDirection: 'row'}}>
-      <FormLabel id="Q2" sx={{width:'50%'}} >{ques[1]}</FormLabel>
-      <RadioGroup
-        row
-        aria-labelledby="demo-row-radio-buttons-group-label"
-        name="row-radio-buttons-group"
-      >
-        <FormControlLabel value="1" control={<Radio />} label="1" labelPlacement='bottom' />
-        <FormControlLabel value="2" control={<Radio />} label="2" labelPlacement='bottom' />
-        <FormControlLabel value="3" control={<Radio />} label="3" labelPlacement='bottom' />
-        <FormControlLabel value="4" control={<Radio />} label="4" labelPlacement='bottom' />
-        <FormControlLabel value="5" control={<Radio />} label="5" labelPlacement='bottom' />
-      </RadioGroup>
-    </FormControl>
-    </div>
-
-
-    <div style={{display: "flex", width: "80%",  fontFamily: "arial", marginBottom: '30px'}}>
-    <FormControl sx={{flexDirection: 'row'}}>
-      <FormLabel id="Q3" sx={{width:'50%'}} >{ques[2]}</FormLabel>
-      <RadioGroup
-        row
-        aria-labelledby="demo-row-radio-buttons-group-label"
-        name="row-radio-buttons-group"
-      >
-        <FormControlLabel value="1" control={<Radio />} label="1" labelPlacement='bottom' />
-        <FormControlLabel value="2" control={<Radio />} label="2" labelPlacement='bottom' />
-        <FormControlLabel value="3" control={<Radio />} label="3" labelPlacement='bottom' />
-        <FormControlLabel value="4" control={<Radio />} label="4" labelPlacement='bottom' />
-        <FormControlLabel value="5" control={<Radio />} label="5" labelPlacement='bottom' />
-      </RadioGroup>
-    </FormControl>
-    </div>
+    )}
     </>
   );
 }
