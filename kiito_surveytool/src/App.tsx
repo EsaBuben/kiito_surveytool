bb1f30403be0b2f99c0fab0cb4b990f01d5afdcf
Esa <esa.buben@student.lab.fi>
2024-04-10 11:12:04 +0300

    PDF pohja

--- kiito_surveytool/src/App.tsx
+++ kiito_surveytool/src/App.tsx
@@ -1,11 +1,16 @@
 import './App.css';
 import FetchJSON from './utils/FetchJSON';
-
+import { PDFViewer } from '@react-pdf/renderer';
+import ReactDOM from 'react-dom';
+import BasicDocument from './components/BasicDocument';
 
 function App() {
-
+  <PDFViewer>
+    <BasicDocument />
+  </PDFViewer>
   return (
     FetchJSON()
+
   )
 }
 
