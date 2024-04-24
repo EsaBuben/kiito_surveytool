f21fdda664e846bd6fecabeae3b7388542cb48da
simokuos <simo.kuosmanen@student.lab.fi>
2024-04-24 16:56:11 +0300

    cleaning

--- kiito_surveytool/src/App.tsx
+++ kiito_surveytool/src/App.tsx
@@ -1,15 +1,11 @@
 import './App.css';
 import FetchJSON from './utils/FetchJSON';
-// import { PDFViewer } from '@react-pdf/renderer';
-// import ReactDOM from 'react-dom';
-// import BasicDocument from './components/BasicDocument';
 
 function App() {
 
   return (
     <div className="App">
       <FetchJSON />
-      <div id="hidden_content"></div>
     </div>
   )
 }
